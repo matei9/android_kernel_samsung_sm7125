56c8b44f90cf2b1c4ea3f82029e9419e65465210
TestaMic <mateidiac9@gmail.com>
2026-04-11 21:58:20 +0300

    susfs: Compile fixes; gitignore updates.

--- fs/open.c
+++ fs/open.c
@@ -1082,6 +1082,7 @@ struct file *filp_clone_open(struct file *oldfile)
 EXPORT_SYMBOL(filp_clone_open);
 
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
+#include <linux/susfs_def.h>
 extern struct filename *susfs_open_redirect_spoof_do_sys_openat(struct inode *inode);
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 
